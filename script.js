5a24cf3c0be1e8fb48272f159f3980003715cf09
Moksh parashar <parasharm239@gmail.com>
2024-04-08 12:39:27 +0530

    .

---
+++ script.js
@@ -0,0 +1,46 @@
+// function func() {
+//   let a = 7;
+//   if (a == 2) {
+//     let num = 2;
+//   } else {
+//     var nam = "Moksh";
+//   }
+//   console.log(nam);
+// }
+//  = 0
+// func();
+
+// function func(a, b) {
+//   const c = a + b;
+//   return c;
+// }
+// const num = func(5, 10);
+// console.log(num);
+// const func = (a, b) => {
+//   let c = a + b;
+//   return c;
+// };
+
+// const num = func(1, 2);
+// console.log(num);
+
+// for (let i; i < 4; i++) {
+//   console.log(i);
+// }
+
+let i = 0;
+while (i < 0) {
+  console.log(i);
+  i++;
+}
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 0);
+
+const arr = [1, 2, 3];
+
+for (const value of arr) {
+  console.log(arr[0]);
+}
